126e4e12569014fc9c9a3511595bfb40d927a09d
Maryam Kia <Maryam.kia1984@gmail.com>
2018-12-12 11:38:52 +0330

    Taskmangoal-4 Complete user object and start to create task object

--- test_user.py
+++ test_user.py
@@ -3,11 +3,9 @@ from with_object import User
 if __name__ == '__main__':
     user_name = 'asghar'
     password = 'akbar'
-    u = User('ali', 'behboudi', user_name, password)
+    u = User('male', 'ali', 'behboudi', user_name, password)
     assert u.password != password, 'Password not hashed!'
-    assert u.authenticate(), 'Authentication failed'
-    password = 'dummy' + password
-    # assert not u.authenticate('dummy' + password), 'Authentication should fail with wrong password!'
-    assert not u.authenticate(), 'Authentication should fail with wrong password!'
+    assert u.authenticate(password), 'Authentication failed'
+    assert not u.authenticate('dummy' + password), 'Authentication should fail with wrong password!'
     print(u.fullname())
     print(u.user_id)
--- with_object.py
+++ with_object.py
@@ -1,48 +1,57 @@
 import hashlib
+import datetime
 
 
 class User:
     user_id = 0
     user_list = {}
+    title_of_gender = {'female': 'Ms.', 'male': 'Mr.'}
 
-    def __init__(self, first_name, last_name, user_name, password):
+    def __init__(self, gender, first_name, last_name, user_name, password):
+        self.gender = gender
         self.first_name = first_name
         self.last_name = last_name
         self.user_name = user_name
-        password = hashlib.sha3_256(password.encode('utf-8')).hexdigest()
-        self.password = password
+        self.password = hashlib.sha3_256(password.encode('utf-8')).hexdigest()
         if user_name not in User.user_list:
             User.user_id += 1
 
-    def authenticate(self):
-        user_list = {self.user_name: self.password}
-        if self.user_name in user_list:
-            if self.password == user_list[self.user_name]:
-                print('Welcome!', User.fullname(self))
-                return True
-            else:
-                print('Invalid username or password')
+    def authenticate(self, new_password):
+        new_password = hashlib.sha3_256(new_password.encode('utf-8')).hexdigest()
+        if new_password == self.password:
+            print('Welcome!', User.fullname(self))
+            return True
+        else:
+            print('Invalid username or password')
 
     def fullname(self):
-        gender = input('what is your gender(Male or Female)? ')
-        if gender == 'female' or gender == 'Female':
-            title = 'Ms.'
-        else:
-            title = 'Mr.'
+        title = self.title_of_gender[self.gender.lower()]
         return '{} {} {}'.format(title, self.first_name, self.last_name)
 
 
 class Task:
-    def __init__(self, user_id, task_id, tak_summary, task_date, progress ):
-        user_id = User.user_id
+    def __init__(self, task_id, task_summary, task_date, progress):
+        self.task_id = task_id
+        self.task_summary = task_summary
+        self.task_date = task_date
+        self.progress = progress
+
+    def create(self):
+        task_date = datetime.datetime.strptime(self.task_date, '%Y/%m/%d')
+        till_date = task_date - datetime.datetime.today()
+        task_info = {'user_id': User.user_id,
+                     'task_id': self.task_id,
+                     'task_summary': self.task_summary,
+                     'task_date': self.task_date,
+                     'task_progress': self.progress,
+                     'task_duration': till_date
+                     }
+        return task_info
 
     def show_task(self):
-        pass
+        print(Task.task_info)
 
-    def save_task(self):
         pass
 
     def load_task(self):
         pass
-
-
