47cbf5571100aa38055c8e9fc04a073ff7ec161d
Maryam Kia <Maryam.kia1984@gmail.com>
2018-12-17 12:09:07 +0330

    create taskmangoal_bin and refactor user_object

--- taskmangoal_bin.py
+++ taskmangoal_bin.py
@@ -4,52 +4,39 @@ import hashlib
 User.read_users()
 
 
-def register(gender, first_name, last_name, user_name, password):
-    for profile in User.user_list:
-        if user_name in profile['user_name']:
-            print('This user is available. please try another one.')
-        else:
-            continue
-
-    user_profile = {}
-    password = hashlib.sha3_256(password.encode('utf-8')).hexdigest()
-    user_profile['gender'] = gender.lower()
-    user_profile['first_name'] = first_name
-    user_profile['last_name'] = last_name
-    user_profile['user_name'] = user_name
-    user_profile['password'] = password
-    print(user_profile)
-    User.user_list.append(user_profile)
-    User.user_id += 1
-    print('Successful Registration')
-
-
-def login(user_name, password):
-    for profile in User.user_list:
-        if user_name not in profile['user_name']:
-            print('Invalid user_name!')
-        else:
-            continue
-    t = [profile for profile in User.user_list if profile['user_name'] == user_name][0]
-    u = User(t['gender'], t['first_name'], t['last_name'], t['user_name'], t['password'])
-    if u.authenticate(password) is True:
-        print('Welcome', u.fullname())
-
-
-login_or_register = input('Dou you want to login or register?')
-if login_or_register == 'login'.lower():
-    un = input('user_name: ')
-    pw = input('password: ')
-    login(un, pw)
-elif login_or_register == 'register'.lower():
+def register():
     gd = input('gender: ')
     fn = input('first_name: ')
     ln = input('last_name: ')
     un = input('user_name: ')
     pw = input('password: ')
-    register(gd, fn, ln, un, pw)
-else:
-    print('please input "login" or "register')
+    return User.create_user(gd, fn, ln, un, pw)
 
-print(User.user_list)
+
+def login():
+    user_name = input('user_name: ')
+    password = input('password: ')
+    if User.user_name_exists(user_name):
+        user = User.get_user(user_name)
+        if user.authenticate(password):
+            return user
+        else:
+            return False
+
+
+active_user = None
+while True:
+    login_or_register = input('Dou you want to login or register?').lower()
+    if login_or_register == 'login':
+        active_user = login()
+
+    elif login_or_register == 'register':
+        active_user = register()
+    else:
+        print('please input "login" or "register')
+
+    if active_user:
+        print('Welcome', active_user.fullname())
+        break
+
 User.save_users()
--- user_object.py
+++ user_object.py
@@ -8,34 +8,54 @@ class User:
     user_list = []
     title_of_gender = {'female': 'Ms.', 'male': 'Mr.'}
 
-    def __init__(self, gender, first_name, last_name, user_name, password):
+    def __init__(self, gender, first_name, last_name, user_name, password, passwoed_is_hashed=False):
         self.gender = gender
         self.first_name = first_name
         self.last_name = last_name
         self.user_name = user_name
-        self.password = hashlib.sha3_256(password.encode('utf-8')).hexdigest()
-        if user_name not in User.user_list:
-            User.user_id += 1
+        self.password = password if passwoed_is_hashed else hashlib.sha3_256(password.encode('utf-8')).hexdigest()
 
-    @staticmethod
-    def read_users():
+    @classmethod
+    def create_user(cls, *args, **kwargs):
+        new_user = cls(*args, **kwargs)
+        if not cls.user_name_exists(new_user):
+            cls.user_list.append(new_user)
+            return new_user
+        else:
+            return False
+
+    def __eq__(self, other):
+        return self.user_name == other.user_name
+
+    @classmethod
+    def get_user(cls, user_name):
+        if cls.user_name_exists(user_name):
+            return [user for user in cls.user_list if user.user_name == user_name][0]
+
+    @classmethod
+    def user_exists(cls, user):
+        return user in cls.user_list
+
+    @classmethod
+    def user_name_exists(cls, user_name):
+        return user_name in [user.user_name for user in cls.user_list]
+
+    @classmethod
+    def read_users(cls):
         if os.path.isfile('user_list.csv'):
-            with open('tasks.csv', newline='') as csvfile:
+            with open('user_list.csv', newline='') as csvfile:
                 reader = csv.DictReader(csvfile)
                 for row in reader:
-                    User.user_list.append(dict(row))
+                    cls.user_list.append(cls(**row, passwoed_is_hashed=True))
 
-        # with open('user_list', 'wb') as handle:
-        #     pickle.dump(User.user_list, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-    @staticmethod
-    def save_users():
+    @classmethod
+    def save_users(cls):
         with open('user_list.csv', 'w', newline='') as csvfile:
             items = ['gender', 'first_name', 'last_name', 'user_name', 'password']
             writer = csv.DictWriter(csvfile, fieldnames=items)
             writer.writeheader()
-            for profile in User.user_list:
-                writer.writerow(profile)
+            for user in cls.user_list:
+                writer.writerow(user.__dict__)
 
     def authenticate(self, new_password):
         new_password = hashlib.sha3_256(new_password.encode('utf-8')).hexdigest()
